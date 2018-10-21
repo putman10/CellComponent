46f1c8c8807b330d0d8e22afef210b31dbebe290
Tim Putman <timeput@gmail.com>
2018-10-21 09:40:03 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      Welcome to my app
+    </div>
+    <router-view/>
+    <router-link
+        to="/away"
+        class="btn btn-dark">
+      <span>Learn More</span>
+    </router-link>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      message2: 'Hi Fisher',
+      list1: ['thing1', 'thing2', 'thing3'],
+      url1: 'http://chlambase.org',
+    }
+  },
+
+  props: {
+    msg: {
+      type: String,
+      required: false,
+      default: "default message"
+
+    }
+  },
+  computed :{
+    newMsg() {
+      return this.msg.length;
+    },
+  },
+  methods: {
+    doSomething(n, i) {
+      return n + i;
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
